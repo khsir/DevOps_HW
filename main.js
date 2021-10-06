b85e2166fa3339ce9d281ed5a4a5d188cdaed399
Kevin Yap <kevin.yap.hs@gmail.com>
2021-10-06 10:12:40 +0800

    trying something different

--- main.js
+++ main.js
@@ -1,43 +1,11 @@
-var baseUrl = "https://api.coinranking.com/v2/coins";
-var proxyUrl = "https://cors-anywhere.herokuapp.com/";
-var apiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+const express = require("express");
 
-var apiUrl = `${proxyUrl}${baseUrl}`;
-console.log(apiUrl);
+var app = express();
 
-fetch(`${proxyUrl}${baseUrl}`, { 
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-My-Custom-Header': `${apiKey}`,
-      'Access-Control-Allow-Origin': "*"
+app.listen(3000,(errors) => {
+    if(errors) {
+        console.log("Server cannot start. Error: "+errors);
+    } else {
+        console.log("Server started on Port:3000")
     }
-})
-  .then((response) => {
-    if (response.ok) {
-      response.json().then((json) => {
-        console.log(json.data);
-        let coinsData = json.data.coins;
-
-        if (coinsData.length > 0) {
-          var cryptoCoin = "";
-        }
-        //For Loop Starts
-        coinsData.forEach((coin) => {
-          cryptoCoin += "<tr>";
-          cryptoCoin += `<td> ${coin.uuid} </td>`;
-          cryptoCoin += `<td> ${coin.btcPrice} </td>`;
-          cryptoCoin += `<td> ${coin.rank}</td>`;
-          cryptoCoin += `<td> ${coin.tier} </td>`;
-          cryptoCoin += `<td> ${coin.name}</td>`;
-          cryptoCoin += `<td> $${Math.round(coin.price)} Billion</td>`;
-          cryptoCoin += `<td> ${coin.symbol}</td>`;"<tr>";
-        });
-        //For Loop Ends
-        document.getElementById("data").innerHTML = cryptoCoin;
-      });
-    }
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+});
